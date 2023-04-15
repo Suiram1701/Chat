673106c012c0f057cdb3b002c15f784312e543f3
Marius Kaufman <110390261+Suiram1701@users.noreply.github.com>
2023-04-15 22:31:50 +0200

    Setup mvvm patern

---
+++ Chat/ViewModel/ViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Chat.ViewModel
+{
+    internal abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Update a property 
+        /// </summary>
+        /// <param name="propertyName">Property to update</param>
+        protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (!string.IsNullOrEmpty(propertyName))
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
