11da27e909fe7947e659e817e236b7f12cb2fab3
Marius Kaufman <110390261+Suiram1701@users.noreply.github.com>
2023-04-23 20:50:26 +0200

    Fix nickname validation, add start btn and selected chat btns

--- Chat/ViewModel/Menu.cs
+++ Chat/ViewModel/Menu.cs
@@ -1,15 +1,96 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Localization;
+using Microsoft.Win32;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using static Chat.Properties.Settings;
 
 namespace Chat.ViewModel
 {
     internal class Menu : ViewModelBase
     {
+        // Localization
+
+
+        public Menu()
+        {
+            // Init commands
+            StartChatCommand = new DelegateCommand(parameter => !HasError(nameof(Nickname)), parameter =>
+            {
+                //TODO: Start new chat
+            });
+
+            LoadChatHistoryCommand = new DelegateCommand(parameter =>
+            {
+                // Select file
+                OpenFileDialog ofd = new OpenFileDialog
+                {
+                    Title = "Select saved chat history",
+                    Filter = "Chat history file (*.chat)|*.chat",
+                    CheckFileExists = true,
+                    Multiselect = false
+                };
+
+                if (ofd.ShowDialog() == true)
+                {
+                    string fileName = ofd.FileName.Substring(ofd.FileName.LastIndexOf('\\') + 1);
+
+                    //TODO: Check if file is a valid file
+                    if (false)
+                    {
+                        MessageBox.Show($"The file {fileName} isn't a valid chat history file!", "Not valid file selected!", MessageBoxButton.OK, MessageBoxImage.Error);
+                        return;
+                    }
+
+                    SelectedFile = ofd.FileName;
+                }
+            });
+
+            NewHistoryCommad = new DelegateCommand(parameter => !string.IsNullOrEmpty(SelectedFile), parameter => SelectedFile = string.Empty);
+        }
+
+        // Commands
+        /// <summary>
+        /// Start a new chat as host
+        /// </summary>
+        public DelegateCommand StartChatCommand { get; }
+
+        /// <summary>
+        /// Load a saved chat history
+        /// </summary>
+        public DelegateCommand LoadChatHistoryCommand { get; }
+
+        public DelegateCommand NewHistoryCommad { get; }
+
         // Data
+        public string SelectedFileView =>
+            !string.IsNullOrEmpty(SelectedFile) ? SelectedFile.Substring(SelectedFile.LastIndexOf('\\') + 1) : "New Chat";
+
+        /// <summary>
+        /// Letters left in nickname input box
+        /// </summary>
+        public string LettersLeft =>
+            "Letters left: " + (20 - Nickname.Length > 0 ? 20 - Nickname.Length : 0);
+
+        public string SelectedFile
+        {
+            get => _SelectedFile;
+            set
+            {
+                if (_SelectedFile != value)
+                {
+                    _SelectedFile = value;
+                    RaisePropertyChanged();
+                    RaisePropertyChanged(nameof(SelectedFileView));
+                    NewHistoryCommad.RaiseCanExecuteChanged();
+                }
+            }
+        }
+        private string _SelectedFile = string.Empty;
+
         /// <summary>
         /// Nickname in chat
         /// </summary>
@@ -20,11 +101,43 @@ namespace Chat.ViewModel
             {
                 if (value != _Nickname)
                 {
+                    #region Validation
+                    ClearErrors();
+
+                    if (value.Length > 20)
+                        AddError("Maximum letters reached!");
+
+                    if (value.Contains(" "))
+                        AddError("Mustn't contain spaces!");
+
+                    if (string.IsNullOrEmpty(value))
+                        AddError("Field cannot be empty!");
+
+                    // Check for invalid letters
+                    StringBuilder sb = new StringBuilder();
+                    foreach (char c in value)
+                        if (!char.IsLetterOrDigit(c) && c != ' ')
+                            if (!sb.ToString().Contains(c.ToString()))
+                                sb.Append($"{c}, ");
+                    if (sb.Length > 0)
+                    {
+                        sb.Remove(sb.Length - 2, 2);
+                        if (sb.Length == 1)
+                            AddError(sb.ToString() + " isn't a valid letter!");
+                        else
+                            AddError(sb.ToString() + " aren't a valid letters!");
+                    }
+
+                    RaiseErrorsChanged();
+                    #endregion
+
                     _Nickname = value;
                     RaisePropertyChanged();
+                    RaisePropertyChanged(nameof(LettersLeft));
+                    StartChatCommand.RaiseCanExecuteChanged();
                 }
             }
         }
-        private string _Nickname = string.Empty;
+        private string _Nickname = Default.Username;
     }
 }
--- Chat/ViewModel/ViewModelBase.cs
+++ Chat/ViewModel/ViewModelBase.cs
@@ -1,11 +1,27 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
 
 namespace Chat.ViewModel
 {
-    internal abstract class ViewModelBase : INotifyPropertyChanged
+    internal abstract class ViewModelBase : INotifyPropertyChanged, INotifyDataErrorInfo
     {
+        public bool HasErrors =>
+            Errors.Count > 0;
+
+        /// <summary>
+        /// Get current View model base
+        /// </summary>
+        public ViewModelBase Error =>
+            this;
+
+        private IDictionary<string, List<string>> Errors { get; } = new Dictionary<string, List<string>>();
+
         public event PropertyChangedEventHandler PropertyChanged;
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
 
         /// <summary>
         /// Update a property 
@@ -15,6 +31,60 @@ namespace Chat.ViewModel
         {
             if (!string.IsNullOrEmpty(propertyName))
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public bool HasError([CallerMemberName] string propertyName = "") =>
+            Errors.ContainsKey(propertyName);
+
+        public IEnumerable GetErrors([CallerMemberName] string propertyName = "")
+        {
+            if (Errors.ContainsKey(propertyName))
+                return Errors[propertyName];
+            else
+                return Array.Empty<string>();
+        }
+
+        /// <summary>
+        /// Get the next error for the property
+        /// </summary>
+        /// <param name="propertyName">Property</param>
+        /// <returns>Error message</returns>
+        public string this[string propertyName]
+        {
+            get => GetErrors(propertyName).Cast<string>().First();
+        }
+
+        /// <summary>
+        /// Add an error to the error list for the property
+        /// </summary>
+        /// <param name="message">Error message</param>
+        /// <param name="propertyName">Property</param>
+        protected virtual void AddError(string message, [CallerMemberName] string propertyName = "")
+        {
+            if (!Errors.ContainsKey(propertyName))
+                Errors.Add(propertyName, new List<string>());
+
+            Errors[propertyName].Add(message);
+        }
+
+        /// <summary>
+        /// Remove all errors for the property
+        /// </summary>
+        /// <param name="propertyName">Property</param>
+        protected virtual void ClearErrors([CallerMemberName] string propertyName = "")
+        {
+            Errors.Remove(propertyName);
+            RaiseErrorsChanged(propertyName);
+        }
+
+        /// <summary>
+        /// Notify that the errors has been changed
+        /// </summary>
+        /// <param name="propertyName">Property</param>
+        protected virtual void RaiseErrorsChanged([CallerMemberName] string propertyName = "")
+        {
+            if (!string.IsNullOrEmpty(propertyName))
+                ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
         }
     }
 }
